bec5b5fbc4691909de21cc2ff5f1ac9935db3232
idah-github <guantaiidah@gmail.com>
2023-04-20 11:10:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	return0;
+/* 
+ * main-also the entry point'
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
