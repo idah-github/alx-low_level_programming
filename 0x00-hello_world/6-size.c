b61015262689a5550b32e0f298c5d7bd5a652e2c
idah-github <guantaiidah@gmail.com>
2023-04-20 21:57:47 -0700

    type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-	printf("Sie of char: %d byte(s)", sizeof(char));
-	printf("Size of int: %d byte(s)", sizeof(int));
-	printf("Size of long int: %d byte(s)", sizeof(long int));
-	printf("Size of float: %d byte(s)", sizeof(float));
-	printf("Size of double: %d byte(s)", sizeof(double));
-	printf("Size of long double: %d byte(s)", sizeof(long double));
+	printf("Sie of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 	return (0);
 }
