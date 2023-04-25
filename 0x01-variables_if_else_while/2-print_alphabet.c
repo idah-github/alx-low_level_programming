9749736a2cf9dead9a7a2d05779bf6709ff6b6be
idah-github <guantaiidah@gmail.com>
2023-04-24 23:32:47 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Discription-cheking if number is positive
+* Return:  always  #
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+		printf("%c", a);
+	return (0);
+}
