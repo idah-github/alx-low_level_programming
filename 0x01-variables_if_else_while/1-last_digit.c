04223e4291488e5e4ebf773f7c4ef5f68364cc1c
idah-github <guantaiidah@gmail.com>
2023-04-24 23:15:43 -0700

    greaterthan

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+* main - entry point
+* Discription-cheking if number is positive
+* Return:  always  #
+*/
+
 int main(void)
 {
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	ld = n % 10;
+	if (ld > 5)
+	{
+		printf("The last digit of %d is %d and is geater than 5", n, ld);
+	}
+	else if (ld == 0)
+	{
+		printf("The last digit of %d is %d and is 0", n, ld);
+	}
+	else
+	{
+		printf("The last digit of %d is %d and is less than 6 and not 0", n, ld);
+}
+
 	return (0);
 }
