53bc4a9bc9628685698e3f3c6325f54dbe60855a
idah-github <guantaiidah@gmail.com>
2023-04-26 10:17:02 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - is the entry point
+* Return:always #
+*/
+int main(void)
+{
+	int c = '_';
+
+	putchar(c);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
