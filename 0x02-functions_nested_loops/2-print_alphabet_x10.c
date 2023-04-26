74721c74a4848d8e5e0f56cb477e881fbee90c93
idah-github <guantaiidah@gmail.com>
2023-04-26 11:18:43 -0700

    alphabet with new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet with new line
+*Return - return void
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
