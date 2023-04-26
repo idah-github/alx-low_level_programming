4221d7c91e134ad3f29af456ba1fa631b9a96678
idah-github <guantaiidah@gmail.com>
2023-04-26 15:29:22 -0700

    numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_to_98 - Check number fromnto98
+*@n: A input integer
+*
+*Return: Numbers
+*/
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	{
+		if (n == 98)
+		{
+			printf("%d\n", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
+}
