00a0fe790a32c27333a7984c1e8b27e4083aacd5
idah-github <guantaiidah@gmail.com>
2023-04-26 14:37:49 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_last_digit - last digit
+*
+*@n: store value/n
+*Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last_dgt = n % 10;
+
+	return (last_dgt);
+}
