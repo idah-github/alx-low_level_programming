338fff57075827f6a828873c5cf3018cfe15249c
idah-github <guantaiidah@gmail.com>
2023-04-27 00:31:54 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main -is the entry point
+*
+* Return: is #
+*/
+int main(void)
+{
+	for (int num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
