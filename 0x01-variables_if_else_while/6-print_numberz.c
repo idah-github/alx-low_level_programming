e7cddb153fb5f27275e490c88953b479fbf1b033
idah-github <guantaiidah@gmail.com>
2023-04-27 00:47:07 -0700

    putchar num

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-/*
+/**
 * main - entry point
 * Return: will be 0
 */
 int main(void)
 {
+	int num;
 
+	for (num  = 0; num <= 9; num++)
+	{
+		putchar(num + 48);
+	}
+	putchar('\n');
+	return (0);
+}
