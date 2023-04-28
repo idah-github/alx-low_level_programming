b818c82762dcbd67ce73b9c91805bd55760da647
idah-github <guantaiidah@gmail.com>
2023-04-28 13:12:12 -0700

    draw triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,21 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- */
+*print_triangle - drwas a triangle
+*
+*@size: the tria size / number it prints
+*/
 void print_triangle(int size)
 {
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (int a = 1; a <= size; a++)
+	{
+		for (int b = 1; b <= a; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
