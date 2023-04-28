b54706587db48202f315853acd92acce95167662
idah-github <guantaiidah@gmail.com>
2023-04-28 00:41:32 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_most_number 
+*
+*
+*
+*
+*
+*/
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+	{
+		if (num = !2 && num = !4)
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+	return(0);
+}
+
