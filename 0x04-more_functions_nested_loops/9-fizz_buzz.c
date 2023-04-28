387d264073109abad10065a2d1fc6f8a5f26a05d
idah-github <guantaiidah@gmail.com>
2023-04-28 12:54:44 -0700

    modulus

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - is the entry point
+* Return: will be 0#
+*/
+int main(void)
+{
+	int fb;
+
+	for (fb = 1; fb <= 100; fb++)
+	{
+		if (fb % 3 == 0)
+			printf("Fizz");
+		else if (fb % 5 == 0)
+			printf("Buzz");
+		else if (fb % 3 == 0 && fb % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", fb);
+		if (fb < 100)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
