7b952c03200c8ce6f1ff40faf4de11c1abc7f808
idah-github <guantaiidah@gmail.com>
2023-04-28 11:49:33 -0700

    digit exempt

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,16 @@
 #include "main.h"
 /**
-*print_most_number 
-*
-*
-*
-*
+*print_most_numbers - print numbers except 2&4
 *
 */
 void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <=9; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (num = !2 && num = !4)
-		{
+		if (num != 2 && num != 4)
 			_putchar('0' + num);
-		}
 	}
 	_putchar('\n');
 }
