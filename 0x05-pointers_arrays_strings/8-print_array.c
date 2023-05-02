99672cbe47bb1376b8a6e250493f1ef999af94b0
idah-github <guantaiidah@gmail.com>
2023-05-02 10:52:59 -0700

    arrelmtprint

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*print_array - prints elements of an arra
+*@a: the array val
+*@n: elements of the array
+*
+*Return: void
+*/
+void print_array(int *a, int n)
+{
+	int arr = a[n];
+
+	printf("%d\n", arr);
+}
+
