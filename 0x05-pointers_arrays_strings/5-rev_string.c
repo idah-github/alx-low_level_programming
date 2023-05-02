09e90573e8edeecd8cf3c7380e11c78a4340fac5
idah-github <guantaiidah@gmail.com>
2023-05-02 12:52:48 -0700

    reverse str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 {
 	int l = strlen(s);
 	int i;
-	char rv = s[i];
 
 	for (i = 0; i < l / 2; i++)
 	{
+		char rv = s[i];
 		s[i] = s[l - i - 1];
 		s[l - i - 1] = rv;
 	}
