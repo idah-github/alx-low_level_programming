a002247e9c7624f6d4d781413d226956025c47f9
idah-github <guantaiidah@gmail.com>
2023-05-02 02:54:36 -0700

    swap 2 numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- */
+* swap_int - swaps the numbers
+* @a: the first int
+* @b: the second int
+* Return: void
+*
+*/
 void swap_int(int *a, int *b)
 {
 	*a = *b;
