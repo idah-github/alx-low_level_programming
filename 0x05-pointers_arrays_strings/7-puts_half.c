5f2642beb8d4cf52d469cfffeec92ad0237b502c
idah-github <guantaiidah@gmail.com>
2023-05-02 10:25:28 -0700

    stringhalf

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
- *
- *
- *
- */
+*puts_half - returns half of a string
+*@str: the ver of the string
+*Return: void
+*/
+void puts_half(char *str)
+{
+	int l = strlen(str);
+	int hl = l / 2;
+	int hs;
+
+	for (hs = 0; hs < hl; hs++)
+	{
+		printf("%c", str[hs]);
+	}
+	printf("\n");
+}
+
 
