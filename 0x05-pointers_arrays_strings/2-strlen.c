8cfed463bf6d5390fa8efd8868a43b91948153af
idah-github <guantaiidah@gmail.com>
2023-05-02 03:12:12 -0700

    return strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-/**
- *
- *
- *
- *
- */
+#include <string.h>
 
+/**
+*_strlen - checks the string length
+*@s: the str var
+*Return: return the str len
+*
+*/
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(*s);
+	return (l);
+}
