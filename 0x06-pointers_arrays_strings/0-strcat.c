ef2a9964c0793a0980eb94269148ea9b43b002e7
idah-github <guantaiidah@gmail.com>
2023-05-04 09:30:36 -0700

    str concanation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strcat - concatenates 2 str
+*@dest: str to append to
+*@src: str to be appended
+*Return: return the str dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
