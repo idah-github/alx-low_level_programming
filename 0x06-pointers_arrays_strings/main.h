c8454a49f1c39fe17b1807a92219094e34888227
idah-github <guantaiidah@gmail.com>
2023-05-04 11:38:57 -0700

    encode str

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
-char *string_toupper(char *s);
-char *cap_string(char *s);
-
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
