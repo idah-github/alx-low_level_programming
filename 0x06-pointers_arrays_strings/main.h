cb4f06f39d03203a570b0bf9e2f22df5b9195e30
idah-github <guantaiidah@gmail.com>
2023-05-04 11:49:39 -0700

    compare trs

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@ char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
+char *_strncpy(char *dest, char *src, int n);
 char *cap_string(char *);
 char *leet(char *);
