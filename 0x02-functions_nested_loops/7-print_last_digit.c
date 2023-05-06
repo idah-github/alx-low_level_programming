8bffc40c459463ee1e879b40e716250cdbac8895
idah-github <guantaiidah@gmail.com>
2023-05-06 01:52:37 -0700

    lastnum digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int last_dgt = n % 10;
+	int last_dgt;
+
+	if (n < 0)
+		n = -n;
+	last_dgt = n % 10;
+
+	if (last_dgt < 0)
+		last_dgt = -last_dgt;
 
 	_putchar (last_dgt + '0');
 	return (last_dgt);
