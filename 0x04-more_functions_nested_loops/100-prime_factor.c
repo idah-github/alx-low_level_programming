e4b41f8ff23bd99b6f42930dbbd1eeab96ebb8b7
idah-github <guantaiidah@gmail.com>
2023-05-06 01:15:04 -0700

    max primefactor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main -thee entry point
-*
+*Return: will be 0#
 */
 int main(void)
 {
+	long int n , max, i;
 
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if ( n > 2)
+		max = n;
+	printf('&ld\n", max);
+	return (0);
+}
