8469ea6083e13599c7fde2b9f72ec11ec1761ec7
idah-github <guantaiidah@gmail.com>
2023-05-07 21:48:47 -0700

    fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
- */
+*main - prints firt 50 Fibonacci numbers
+*
+*Return: #0
+*/
 int main(void)
 {
+	long int n, nxt;
+	long int f = 1;
+	long int s = 2;
+
+	for (n = 1; n <= 50; n++)
+	{
+		if (f != 20365011074)
+		{
+			printf("%ld, ", f);
+		}
+		else
+		{
+			printf("%ld\n", f);
+		}
+		nxt = f + s;
+		f = s;
+		s = next;
+	}
+	return (0);
+}
