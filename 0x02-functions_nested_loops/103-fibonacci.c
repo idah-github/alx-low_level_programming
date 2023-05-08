f5cbcbabf1863ed6f9192578f4774a4416ad9884
idah-github <guantaiidah@gmail.com>
2023-05-07 23:43:04 -0700

    even fibonacci sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,30 @@
+#include < stdio.h>
+/**
+*main - entry point
+*prints the sum of even number in fibonaccii
+*
+*Return: #0
+*/
+int main(void)
+{
+	int i;
+	unsigned long int f, s, nxt, sum;
 
+	f = 1;
+	s = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (f < 400000 && (f % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		nxt = f + k;
+		f = s;
+		s = nxt;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
