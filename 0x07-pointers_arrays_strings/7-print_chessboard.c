82e53a72f831c783233523f9a511e338b3bfaade
idah-github <guantaiidah@gmail.com>
2023-05-08 06:52:33 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,13 +8,13 @@ void print_chessboard(char (*a)[8])
 {
 	int x, y;
 
-	for (x = 'A'; x <= 'H'; x++)
+	for (x = 1; x <= 8; x++)
 	{
 		for (y = 1; y <= 8; y++)
 		{
 			_putchar(a[x][y]);
 		}
-		if (y == 2)
+		/*if (y == 2)*/
 		_putchar('\n');
 	}
 }
