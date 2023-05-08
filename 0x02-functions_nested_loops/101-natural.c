27500c4b1d464e233f96cbfc574b01ce3c961cc6
idah-github <guantaiidah@gmail.com>
2023-05-07 20:53:46 -0700

    sum of multiples of 3and5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - the entry point; calculates the
+*sum of multiples of 3 and 5 below 1024
+*
+*Return: #0
+*/
+int main(void)
+{
+	unsigned long int sum, num1, num2;
+	int i = 0;
+
+	num = 0;
+	num1 = 0;
+	num2 = 0;
+
+	for (; i < 1024; 1++)
+	{
+		if ((i % 3) == 0)
+		{
+			num2 = num2 + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			num1 = num1 + i;
+		}
+	}
+	sum = num2 + num1;
+	printf("%lu\n", sum);
+	return (0);
+}
