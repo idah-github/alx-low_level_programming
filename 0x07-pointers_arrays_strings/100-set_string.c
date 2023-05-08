34674c374491f011c2e13e33a07b693990fa205b
idah-github <guantaiidah@gmail.com>
2023-05-08 10:01:41 -0700

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#incude "main.h"
+#include <stdio.h>
+/**
+*set_string - double pointer
+*@s: pointer to pointer to
+*@to: chaar pointer
+*
+*/
+void set_string(char **s, char *to)
+{
+	char c;
+
+	to = &c;
+	s = &to;
+	s =to;
+}
