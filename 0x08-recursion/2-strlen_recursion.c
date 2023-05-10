8c48e4286893a8e54928e9254787586e3d3e6c84
idah-github <guantaiidah@gmail.com>
2023-05-10 02:40:04 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strlen_recursion - finds the len of a str
+*@s: the str
+*Return: the length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
