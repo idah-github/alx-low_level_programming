6f227c18d396a8cfbadd3da30bee9853556aad9c
idah-github <guantaiidah@gmail.com>
2023-05-11 21:33:06 -0700

    counts arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the agr count
+*@argc: counts arg
+*@argv: stores arg
+*Return: return 0 if no error
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
