b7dc3042cbb14e01c3d9c4e8d11300d85f3320c2
idah-github <guantaiidah@gmail.com>
2023-05-11 21:27:35 -0700

    return progname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - return the program name
+*@argc: counts arg passed
+*@argv: stotres the arg in arr
+*Return: int #0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
