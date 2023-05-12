fb905720f0a792a592077f8673999cd4aa58aa89
idah-github <guantaiidah@gmail.com>
2023-05-12 02:33:43 -0700

    printatgv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
 *@argv: stotres the arg in arr
 *Return: int #0
 */
-int main(int argc __attribute__((unused)), char *argv[])
+int main( __attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
