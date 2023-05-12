29d5b08a4d8ca60ccaa53d75459d08e866bf86a4
idah-github <guantaiidah@gmail.com>
2023-05-12 09:02:02 -0700

    argsum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,8 +12,11 @@ int main(int argc, char *argv[])
 	int c, sum = 0;
 
 	if (argc == 1)
+	{
 		printf("0\n");
-
+		return (0);
+	}
+	
 	for (c = 1; c < argc; c++)
 	{
 		if (*argv[c] >= '0' && *argv[c] <= '9')
