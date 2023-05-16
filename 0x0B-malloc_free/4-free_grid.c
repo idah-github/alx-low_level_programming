e5171441bba3d1e70fd2d56a2efb7ee275278881
idah-github <guantaiidah@gmail.com>
2023-05-16 11:24:54 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*free_grid - frees a 2 dimensional grid previously created
+*by your alloc_grid function.
+*@grid: grid being freed
+*@height: height of the grid
+*
+*/
+void free_grid(int **grid, int height)
+{
+	int g = 0;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (; g < height; g++)
+	{
+		free(grid[g]);
+	}
+}
