94cee70a4d4a746f66a5b0e91e7e64d7bc70ebb0
idah-github <guantaiidah@gmail.com>
2023-05-22 03:47:07 -0700

    printstruc

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,8 +16,6 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->name = "(nil)";
-	else
-	{
+	/*else*/
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	}
 }
