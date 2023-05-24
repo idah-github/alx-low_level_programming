e408aa913923cbfbb125fa3a52040fa36577c1a0
idah-github <guantaiidah@gmail.com>
2023-05-24 03:56:42 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+*print_name - print name
+*@name: the name to be printed
+*@f: pointer to function
+*
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+	{
+		return;
+	}
+	f(name);
+}
