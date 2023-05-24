d186d7bc16842e50b4fea1c631371474157f4036
idah-github <guantaiidah@gmail.com>
2023-05-24 04:55:59 -0700

    functionpointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+*int_index - search for an integer
+*@array: the arr
+*@size: size of the arr
+*@cmp: arr funct pointer
+*Return: 0,1 / -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i = 0;
+
+	if (size <= 0 || !array || !cmp)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (; i <= size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
