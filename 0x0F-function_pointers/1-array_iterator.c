4cfcf39972351b14e6f66c4c8f397d3e2532da38
idah-github <guantaiidah@gmail.com>
2023-05-24 04:17:41 -0700

    arr funtion pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+*array_iterator - array function par
+*@array: the arr
+*@size: the size of the arr
+*@action: the pointer to function
+*
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(*array);
+		i++;
+	}
+}
