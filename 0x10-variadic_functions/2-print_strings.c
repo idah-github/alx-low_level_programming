b28912f530d89431b04683e2971445178649c270
idah-github <guantaiidah@gmail.com>
2023-05-25 10:33:42 -0700

    variadic strprint

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_strings - prints trs
+*@separator: separator btwn str
+*@n: number of strs /arg
+*
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	char *str;
+
+	va_list strs;
+
+	va_start(strs, n);
+
+	for (; i < n; i++)
+	{
+		str = va_arg(strs, char *);
+		if (!str)
+			str = "(nil)";
+		else
+			printf("%s", str);
+		if (!separator && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(strs);
+}
