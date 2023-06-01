9ed4d779fc5aa6fc35c7a74352e3724e85c3993d
idah-github <guantaiidah@gmail.com>
2023-06-01 01:16:27 -0700

    mem realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_realloc - reallocate mem block using mallco and free
+*@ptr: pointer to prev malloc
+*@old_size: size of mem for ptr
+*@new_size: size of new mem block
+*Return: pointer to new mem block
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr0, *ptr1;
+	unsigned int i = 0;
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr1 = malloc(new_size);
+		if (!ptr1)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	ptr0 = ptr;
+	if (new_size < old_size)
+	{
+		for (; i < new_size; i++)
+			ptr1[i] = ptr0[i];
+	}
+	if (new_size > old_size)
+	{
+		for (; i < old_size; i++)
+			ptr1[i] = ptr0[i];
+	}
+	free(ptr);
+	return (ptr1);
+}
+
+
