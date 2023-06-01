b6300d740a91ea46441db0824fb6276df13428b6
idah-github <guantaiidah@gmail.com>
2023-06-01 01:48:52 -0700

    debug

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,23 +9,33 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr0, *ptr1;
+	char *ptr0;
+	char *ptr1;
 	unsigned int i = 0;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-	{
-		ptr1 = malloc(new_size);
-		if (!ptr1)
-			return (NULL);
-		return (ptr);
-	}
-	if (new_size == old_size)
-		return (ptr);
+	if (!ptr)
+		return (malloc(new_size));
+	ptr1 = malloc(new_size);
+	if (!ptr)
+		return (NULL);
+	/*
+	*if (ptr == NULL)
+	*{
+		*ptr1 = malloc(new_size);
+		*if (!ptr1)
+			*return (NULL);
+		*return (ptr);
+	*}
+	*if (new_size == old_size)
+		*return (ptr);
+		*/
 	ptr0 = ptr;
 	if (new_size < old_size)
 	{
@@ -33,12 +43,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			ptr1[i] = ptr0[i];
 	}
 	if (new_size > old_size)
-	{
 		for (; i < old_size; i++)
-			ptr1[i] = ptr0[i];
-	}
+			ptr1[i] = ((char *)ptr)[i];
 	free(ptr);
 	return (ptr1);
 }
-
-
