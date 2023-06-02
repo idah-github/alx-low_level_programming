a03fcfee00111e7127379d85e390460c6c9d9dfe
idah-github <guantaiidah@gmail.com>
2023-06-02 03:26:39 -0700

    node0 add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+*add_node - add node to begining oflist
+*@head: pointer to head pointer
+*@str: str for new node
+*
+*Return: new node address
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n;
+	unsigned int len;
+
+	for (len = 0; str[len]; len++)
+
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+	n->str = strdup(str);
+	n->len = len;
+	n->next = (*head);
+	(*head) = n;
+
+	return (*head);
+}
