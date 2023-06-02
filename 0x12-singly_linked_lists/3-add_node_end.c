498e399a5d6dbb3afe8418ac2c3ba0cecff821a2
idah-github <guantaiidah@gmail.com>
2023-06-02 04:18:37 -0700

    debug

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,13 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+*add_node_end - add node at end of lis
+*@head: pointer to list 
+*@str: string to add
 *
-*
-*
-*
-*
-*
+*Return: the address of new element
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	
+	list_t *n;
+	lis_t *temp = *head;
+	unsigned int len;
+
+	for (len = 0; (str[len]); len+);
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+
