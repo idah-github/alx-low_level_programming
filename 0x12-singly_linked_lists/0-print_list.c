1c0b3add5ecd1f6787f24907d975a01df318b2d7
idah-github <guantaiidah@gmail.com>
2023-06-02 02:24:31 -0700

    linked list debug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,11 @@
 size_t print_list(const list_t *h)
 {
 	size_t c = 0;
-	char *str;
+	/*char *str;*/
 
 	while (h)
 	{
-		if (h == NULL || str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
