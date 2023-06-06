4c5910db8b9de13011b283a4195216e84022841e
idah-github <guantaiidah@gmail.com>
2023-06-05 22:58:38 -0700

    nthnode

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
+*get_nodeint_at_index - return list node
+*@head: pointer to head of list
+*@index: the index of node
 *
-*
-*
-*
-*
+*Return: the node at index(n)
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *tmp = head;
 
+	for (; tmp && i < index; i++, tmp = tmp->next)
+	{
+		if (tmp && i == index)
+			return (tmp);
+		else
+			return (NULL);
+	}
+}
