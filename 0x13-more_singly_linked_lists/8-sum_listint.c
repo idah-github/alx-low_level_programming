6b48a55a645da3e592a3600e8f83c645bf9c8a9a
idah-github <guantaiidah@gmail.com>
2023-06-05 23:19:03 -0700

    listadd

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*sum_listint - calc the sum or list ele
+*@head: pointer to head of list
+*
+*Return: the sum
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *tmp = head;
+
+	while (tmp)
+	{
+		sum += tmp->next;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
