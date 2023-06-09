ddc854032042f7cfcffba121a547f7150bacb397
idah-github <guantaiidah@gmail.com>
2023-06-09 04:30:38 -0700

    convert binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*binary_to_uint - convert binary to unsigned int
+*@b: pointer to 0,1 store
+*
+*Return: converted number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dnum;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (; *b != '\0'; b++)
+		if (*b != '0' && *b != '1')
+			return (0);
+	dnum = (dnum << 1) + (*b - '0');
+
+	return (dnum);
+}
+
