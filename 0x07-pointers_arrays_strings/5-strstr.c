9738b2425b972eb3b1a618ddccf4ec58409fa863
idah-github <guantaiidah@gmail.com>
2023-09-19 11:28:24 -0700

    debug

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,27 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
+
 /**
-*_strstr - locate substring
-*@haystack:  str to search in
-*@needle: sbustring
-*
-*Return: pointer to substring or NULL
-*/
+ * _strstr - function finds the first occurrence of the substring
+ * @haystack: string to be checked
+ * @needle: substring to be checked
+ * Return: needle or NULL
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	i = 0;
-	while (haystack[i] != '\0')
+	for (; haystack[i] != '\0'; haystack++)
 	{
-		j = 0;
-		while (needle[j] != '\0')
+		while (haystack[i] == needle[j] && needle[j] != '\0')
 		{
-			if (haystack[i + j] != needle[j])
-				j++;
-			break;
+			i++;
+			j++;
 		}
-		if (!needle[j])
-			return (&haystack[i]);
-		i++;
+		if (needle[j] == '\0')
+		{
+			return (haystack);
+		}
 	}
 	return (NULL);
 }
-
